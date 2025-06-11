3e995250daca334f2453e6856b09b00a07bbdd5a
Letícia França <notes.letty@gmail.com>
2025-06-11 09:25:37 -0300

    fix: correção de campos do client

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'letty/biblioteca-musical-api',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => 'b0d7b3f738fa472df8a4a6c6f2acdf7bbea20b9b',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'letty/biblioteca-musical-api' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => 'b0d7b3f738fa472df8a4a6c6f2acdf7bbea20b9b',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
